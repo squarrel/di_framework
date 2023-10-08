274a257c8c4670e217f68be391f4e28c14307327
Milos <milosbulatovic3@gmail.com>
2023-10-08 12:10:43 +0200

    invoke container. start and stop its processes. call mail from container

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
+from container import Container
 from database import Db
-import notification import Mail
-from payment import Payment
-from shop import Cart, Item
+from notification import Mail
+from payment import Card, Payment
+from shop import Cart, Item, User
 
 
 if __name__ == '__main__':
-    db = Db('127.0.0.1', 5432, 'user', 'passwd')
-    db.connect()
-    mail = Mail('127.0.0.1', 993, 'user', 'passwd')
-    mail.connect()
+    container = Container()
+    container.open()
+
+    user = User('Milos', 'Bulatovic', 'Street 38', '00381234567', 'milos@mail.com')
 
     item_1 = Item(item_id=124, color='red', amount=2, price=2.44)
     item_2 = Item(item_id=56, color='blue', amount=4, price=1.22)
     item_3 = Item(item_id=88, color='white', amount=5, price = 4.00)
 
     cart = Cart([item_1, item_2, item_3])
-    card = Card(number, first_name, last_name, address)
-    payment = Payment(cart, card, db)
+    card = Card(1234566543211234, 'Milos', 'Bulatovic', 'Address 58')
+    payment = Payment(container, cart, card)
 
     charged = payment.charge()
     if charged:
         msg = 'Successful purchase!'
-        mail.send(user.email_address, msg)
+        container.mail.send(user.email, msg)
+
+    container.close()
