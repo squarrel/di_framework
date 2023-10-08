f9b231b2574abd69a4e94b6ae0fc8a3b822a2dca
Milos <milosbulatovic3@gmail.com>
2023-10-08 12:06:03 +0200

    subclass Vacuum. add start and stop methods

--- database.py
+++ database.py
@@ -1,15 +1,27 @@
-class Db:
-    def __init__(self, address: str, port: int, username: str, password: str):
-        self.address = address
-        self.port = port
-        self.username = username
-        self.password = password
+from abstract import Vacuum
 
-    def connect(self):
+
+class Db(Vacuum):
+    def __init__(self, container):
+        self.container = container
+
+    def start(self):
+        '''Connects the database.'''
+        address = '127.0.0.1'
+        port = 5432
+        username = 'user'
+        password = 'passwd'
+        print('Connected database.')
+        return True
+
+    def stop(self):
+        '''Disconnects database.'''
+        print('Disconnected database.')
         return True
 
     def get(self, table_name: str):
         return {}
 
     def insert(self, table_name: str, *args):
+        print('Inserting arguments {} into table {}.'.format(args, table_name))
         return True
