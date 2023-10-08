9425bffd0fd5e278ce8b8531561095bdeb0e68a3
Milos <milosbulatovic3@gmail.com>
2023-10-08 12:09:41 +0200

    add Item and User class. fix get_items()

--- shop.py
+++ shop.py
@@ -1,14 +1,4 @@
-class Cart:
-    def __init__(self, items: List[Item]):
-        self.items = items
-
-    def get_items(self):
-        return items
-
-    def calc_cost(self):
-        cost = 0
-        for item in self.items:
-            cost += item.price * item.amount
+from typing import List
 
 
 class Item:
@@ -17,3 +7,29 @@ class Item:
         self.color = color
         self.amount = amount
         self.price = price
+
+    def get_price(self):
+        return 15.00
+
+
+class Cart:
+    def __init__(self, items: List[Item]):
+        self.items = items
+
+    def get_items(self):
+        return [item.item_id for item in self.items]
+
+    def calc_cost(self):
+        cost = 0
+        for item in self.items:
+            cost += item.price * item.amount
+        return round(cost)
+
+
+class User:
+    def __init__(self, first_name: str, last_name: str, address: str, phone: int, email: str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address =  address
+        self.phone = phone
+        self.email = email
