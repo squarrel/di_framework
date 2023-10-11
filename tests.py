4593a04b1a592f0dc581522a822cac7a28a8a78c
Milos <milosbulatovic3@gmail.com>
2023-10-11 06:14:31 +0200

    add test add_new_dependency_not_vacuum_subclass

--- tests.py
+++ tests.py
@@ -57,3 +57,35 @@ def test_adding_new_dependency(user):
 
     payment = Payment(cart, card)
     assert isinstance(payment.c.user, user)
+
+
+@pytest.fixture
+def nosql_db():
+    class NoSQLDb:
+        def __init__(self, container):
+            self.c = container
+
+        def start(self):
+            pass
+
+        def stop(self):
+            pass
+
+    dependencies.append(('NoSQLDb', NoSQLDb))
+
+    return NoSQLDb
+
+
+def test_adding_new_dependency_not_vacuum_subclass(nosql_db):
+    item_1 = Item(item_id=124, color='red', amount=2, price=2.44)
+    item_2 = Item(item_id=56, color='blue', amount=4, price=1.22)
+    item_3 = Item(item_id=88, color='white', amount=5, price = 4.00)
+
+    cart = Cart([item_1, item_2, item_3])
+    card = Card(1234566543211234, 'Milos', 'Bulatovic', 'Address 58')
+
+    with pytest.raises(
+        ValueError,
+        match='Container dependencies should be instances of Vacuum class.'
+    ):
+        payment = Payment(cart, card)
